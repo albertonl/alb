1aecc363b3d35882174a55e37ae530bedb933817
Alberto Navalon <alberto.navalonlillo@gmail.com>
2019-05-20 10:55:12 +0200

    Initial commit

---
+++ include/alb-program.hpp
@@ -0,0 +1,54 @@
+/*
+	Program class and reserved keywords
+	The ALB Programming Language
+	Alberto Navalon Lillo (C) 2019
+
+	This software is under the GNU General Public License v3.0
+	Visit https://github.com/albertonl/alb/LICENSE for further details
+*/
+
+#ifndef ALB_PROGRAM_HPP
+#define ALB_PROGRAM_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <string>
+
+#include "../include/alb-statement.hpp" // Program class and reserved keywords
+
+using namespace std;
+using namespace alb_statement;
+
+namespace alb_program{
+	// Reserved keywords
+	enum keywords{
+		GENERAL,
+		BEGIN,END
+	}
+
+	// Program class
+	class Program{
+		private:
+			std::vector<Statement> statements;
+		public:
+			Program(){} // Constructor
+			void readSource(const string fileName); // Read source code into vector
+			// This function can be executed slowly, specially in large files
+	};
+
+	/*
+
+		TODO:
+			- Create readSource() function (it must read insert every token in the file into separate locations in the vector, in the order they appear in the source file)
+
+	*/
+}
+
+#endif // ALB_PROGRAM_HPP
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <string>
+
+#include "../include/alb-statement.hpp" // Statement structure
+#include "../include/alb-program.hpp" // Program class and reserved keywords
+using namespace std;
+using namespace alb_statement;
+using namespace alb_program;
+
+int main(int argc, char const *argv[]){
+	Program program;
+	const string fileName = argv[1];
+
+	program.readSource(fileName); // Still not implemented
+	
+	return 0;
+}
