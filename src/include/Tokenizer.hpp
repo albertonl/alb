d52deb2b145a002ac06df8c54b23d7073d83904c
Adrijaned <adrijaned@gmail.com>
2019-06-10 20:39:03 +0200

    Create Tokenizer

---
+++ src/include/Tokenizer.hpp
@@ -0,0 +1,20 @@
+/*
+	The ALB Programming Language
+	Alb Developers Team (C) 2019
+
+	This software is distributed under the MIT license
+	Visit https://github.com/albertonl/alb/LICENSE for further details
+*/
+#ifndef ALB_TOKENIZER_HPP
+#define ALB_TOKENIZER_HPP
+
+namespace alb_lang {
+  /**
+   * Performs tokenization of input data (parsing the text).
+   */
+  class Tokenizer {
+
+  };
+}
+
+#endif //ALB_TOKENIZER_HPP
