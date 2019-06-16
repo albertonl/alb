3477af4178ba6fc160cb11aa131eb7f79eb26033
Adrijaned <adrijaned@gmail.com>
2019-06-16 15:03:49 +0200

    Added comment handling test

--- tests/src/include/Lexer.cpp
+++ tests/src/include/Lexer.cpp
@@ -19,3 +19,13 @@ TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_token_count_Test) {
   alb_lang::Lexer::tokenizeUTF8(testData, sizeof(testData), tokens);
   ASSERT_EQ(tokens.size(), 3);
 }
+
+TEST(tokenizer_tests, tokenizer_tests_utf8_parsing_comment_Test) {
+  char testData[] = "BEGIN .\n// This is a comment\nEND";
+  std::vector<alb_lang::Token*> tokens;
+  alb_lang::Lexer::tokenizeUTF8(testData, sizeof(testData), tokens);
+  ASSERT_EQ(tokens.size(), 3) << "Third token is \"" << tokens[2]->getTextContents() << "\"";
+  EXPECT_EQ(tokens[0]->getTextContents(), std::string{"BEGIN"});
+  EXPECT_EQ(tokens[1]->getTextContents(), std::string{"."});
+  EXPECT_EQ(tokens[2]->getTextContents(), std::string{"END"});
+}
