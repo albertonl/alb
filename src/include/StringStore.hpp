9742bea87be1a8d0b57e364b87fe81e51d9a1b78
Adrijaned <adrijaned@gmail.com>
2019-06-20 17:19:08 +0200

    Created StringStore

---
+++ src/include/StringStore.hpp
@@ -0,0 +1,20 @@
+/*
+	The ALB Programming Language
+	Alb Developers Team (C) 2019
+
+	This software is distributed under the MIT license
+	Visit https://github.com/albertonl/alb/LICENSE for further details
+*/
+#ifndef ALB_STRINGSTORE_HPP
+#define ALB_STRINGSTORE_HPP
+
+namespace alb_lang {
+  /**
+   * Provides a storage for all string literals in compilation target.
+   */
+  class StringStore {
+
+  };
+}
+
+#endif //ALB_STRINGSTORE_HPP
