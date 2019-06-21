3e76662cb04213a582e5538a690217461f6ab6d7
Adrijaned <adrijaned@gmail.com>
2019-06-21 22:42:47 +0200

    Added definition for storeString

--- src/include/StringStore.hpp
+++ src/include/StringStore.hpp
@@ -9,12 +9,15 @@
 #define ALB_STRINGSTORE_HPP
 
 #include <string>
+#include <vector>
 
 namespace alb_lang {
   /**
    * Provides a storage for all string literals in compilation target.
    */
   class StringStore {
+  private:
+    std::vector<std::string> strings {};
   public:
     /**
      * Returns the amount of currently stored string literals.
@@ -30,7 +33,7 @@ namespace alb_lang {
      * @param s String to store.
      * @return The newly assigned global id of the stored string literal.
      */
-    std::string storeString(std::string s);
+    std::string storeString(const std::string& s);
     /**
      * Returns the string literal stored under specific id.
      *
@@ -39,6 +42,11 @@ namespace alb_lang {
      */
     std::string getString(std::string id) const;
   };
+
+  std::string StringStore::storeString(const std::string& s) {
+    strings.push_back(s);
+    return "::internal::strings::" + std::to_string(strings.size() - 1);
+  }
 }
 
 #endif //ALB_STRINGSTORE_HPP
