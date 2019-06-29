fec8f4d1e64c7cb41cd74f0371174647fef82fce
Adrijaned <adrijaned@gmail.com>
2019-06-29 20:11:46 +0200

    Made CompilerDirectivesChecks into one file

--- src/include/CompilerDirectiveChecks.hpp
+++ src/include/CompilerDirectiveChecks.hpp
@@ -11,8 +11,10 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <absl/strings/str_split.h>
 
 namespace alb_lang {
+
   /**
    * Asserts that the string passed in represents a valid compiler directive,
    * i.e. begins with `::`
@@ -44,5 +46,38 @@ namespace alb_lang {
    * @throws std::runtime_error on failure
    */
   void assertDirectiveIsInternal(const std::vector<std::string> &directiveParts) noexcept(false);
+
+  //----------------------------------------------------------------------------
+  // Implementations below this point
+  //----------------------------------------------------------------------------
+
+  void assertDirectiveIsValidDirective(const std::string &directive) noexcept(false) {
+    assertDirectiveIsValidDirective(absl::StrSplit(directive, "::", absl::AllowEmpty{}));
+  }
+
+  void assertDirectiveIsValidDirective(const std::vector<std::string> &directiveParts) noexcept(false) {
+    // size check disallows size of 1 too - empty string split by `::` would
+    // still have one element in the vector of zero width, but still would not be
+    // a valid compiler directive
+    if (directiveParts.size() < 2 || !directiveParts[0].empty()) {
+      throw (std::runtime_error{
+          "Given string is not a valid compiler directive (compiler directives must begin with `::`)"});
+    }
+  }
+
+  void assertDirectiveIsInternal(const std::string &directive) noexcept(false) {
+    assertDirectiveIsInternal(absl::StrSplit(directive, "::", absl::AllowEmpty{}));
+  }
+
+  void assertDirectiveIsInternal(const std::vector<std::string> &directiveParts) noexcept(false) {
+    assertDirectiveIsValidDirective(directiveParts);
+    // size check disallows size of 2 too - string `::internal` on its own is
+    // still not a valid internal compiler directive
+    if (directiveParts.size() < 3 || directiveParts[1] != "internal") {
+      throw (std::runtime_error{
+          "Given string is not a valid compiler directive (compiler directives must begin with `::`)"});
+    }
+  }
+
 }
 #endif //ALB_COMPILERDIRECTIVECHECKS_HPP
